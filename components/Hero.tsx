3bfb80eaba2d87c7b6c4790f0552895f5892f559
OmMahen <102027819+OmMahen@users.noreply.github.com>
2024-06-25 18:11:51 +0800

    Fix Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ export function Hero() {
   const [user, setUser] = useContext(UserContext);
 
   return (
-    <section className="text-center w-full p-24">
+    <section className="text-center w-full p-24 dark:bg-gray-800">
       {user ? (
         <h1 className="text-5xl font-bold text-green-600">
           Hallo {user.email} !
@@ -16,7 +16,7 @@ export function Hero() {
           Explore Lombok with Travel Yuks
         </h1>
       )}
-      <p className="text-gray-600 mt-4">
+      <p className="mt-4">
         Travel Yuks Terdepan Dalam Melayani Anda
       </p>
     </section>
