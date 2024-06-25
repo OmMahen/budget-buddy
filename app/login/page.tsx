663223794c34ee114eb27883358a48abdf7c09f7
OmMahen <102027819+OmMahen@users.noreply.github.com>
2024-06-25 19:02:10 +0800

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@ import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { SubmitButton } from "./submit-button";
+import { TextInput } from "flowbite-react";
 
 export default function Login({
   searchParams,
@@ -74,12 +75,11 @@ export default function Login({
         Back
       </Link>
 
-      <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+      <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground text-black dark:text-white">
         <label className="text-md" htmlFor="email">
           Email
         </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+        <TextInput
           name="email"
           placeholder="you@example.com"
           required
@@ -87,8 +87,7 @@ export default function Login({
         <label className="text-md" htmlFor="password">
           Password
         </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+        <TextInput
           type="password"
           name="password"
           placeholder="••••••••"
@@ -96,14 +95,14 @@ export default function Login({
         />
         <SubmitButton
           formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+          className="bg-green-700 hover:bg-green-600 rounded-md px-4 py-2 mb-2"
           pendingText="Signing In..."
         >
           Sign In
         </SubmitButton>
         <SubmitButton
           formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+          className="border dark:border-white hover:bg-gray-950 rounded-md px-4 py-2 mb-2"
           pendingText="Signing Up..."
         >
           Sign Up
