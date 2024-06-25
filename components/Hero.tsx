a8ed087d46ea8c1cde766cfa06936b7307b7784f
OmMahen <102027819+OmMahen@users.noreply.github.com>
2024-06-25 18:29:10 +0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,14 +5,18 @@ import { useContext } from "react";
 export function Hero() {
   const [user, setUser] = useContext(UserContext);
 
+  const getEmailName = (email: any) => {
+    return email.split('@')[0];
+  };
+
   return (
-    <section className="text-center w-full p-24 dark:bg-gray-800">
+    <section className="text-center w-full py-24 px-4 dark:bg-gray-800">
       {user ? (
-        <h1 className="text-5xl font-bold text-green-600">
-          Hallo {user.email} !
+        <h1 className="text-3xl md:text-5xl font-bold text-green-600">
+          Hallo {getEmailName(user.email)}!
         </h1>
       ) : (
-        <h1 className="text-5xl font-bold text-green-600">
+        <h1 className="text-3xl md:text-5xl font-bold text-green-600">
           Explore Lombok with Travel Yuks
         </h1>
       )}
